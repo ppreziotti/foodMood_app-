c9e20c161e8d7ba2dca0a6842895cced986f8e14
Eyad Qassem <eyadqassem@Eyads-MacBook-Air.local>
2017-04-11 18:22:17 -0400

    updated facebook login button and like button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,85 @@
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
-  if (response.status === 'connected') {
-    // Logged into your app and Facebook.
-    testAPI();
-  } else {
-    // The person is not logged into your app or we are unable to tell.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
-  }
-}
-
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-window.fbAsyncInit = function() {
-FB.init({
-  appId      : '1678163075543673',
-  cookie     : true,  // enable cookies to allow the server to access
-                      // the session
-  xfbml      : true,  // parse social plugins on this page
-  version    : 'v2.8' // use graph api version 2.8
-});
-
-// Now that we've initialized the JavaScript SDK, we call
-// FB.getLoginStatus().  This function gets the state of the
-// person visiting this page and can return one of three states to
-// the callback you provide.  They can be:
+// //FACEBOOK INTEGRATION
+// //====================================================================================
+// function statusChangeCallback(response) {
+//   console.log('statusChangeCallback');
+//   console.log(response);
+//   // The response object is returned with a status field that lets the
+//   // app know the current login status of the person.
+//   // Full docs on the response object can be found in the documentation
+//   // for FB.getLoginStatus().
+//   if (response.status === 'connected') {
+//     // Logged into your app and Facebook.
+//     testAPI();
+//   } else {
+//     // The person is not logged into your app or we are unable to tell.
+//     document.getElementById('status').innerHTML = 'Please log ' +
+//       'into this app.';
+//   }
+// }
 //
-// 1. Logged into your app ('connected')
-// 2. Logged into Facebook, but not your app ('not_authorized')
-// 3. Not logged into Facebook and can't tell if they are logged into
-//    your app or not.
+// // This function is called when someone finishes with the Login
+// // Button.  See the onlogin handler attached to it in the sample
+// // code below.
+// function checkLoginState() {
+//   FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+//   });
+// }
 //
-// These three cases are handled in the callback function.
-
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-  console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-      'Thanks for logging in, ' + response.name + '!';
-      userName = response.name;
-  });
-}
+// window.fbAsyncInit = function() {
+// FB.init({
+//   appId      : '1678163075543673',
+//   cookie     : true,  // enable cookies to allow the server to access
+//                       // the session
+//   xfbml      : true,  // parse social plugins on this page
+//   version    : 'v2.8' // use graph api version 2.8
+// });
+//
+// // Now that we've initialized the JavaScript SDK, we call
+// // FB.getLoginStatus().  This function gets the state of the
+// // person visiting this page and can return one of three states to
+// // the callback you provide.  They can be:
+// //
+// // 1. Logged into your app ('connected')
+// // 2. Logged into Facebook, but not your app ('not_authorized')
+// // 3. Not logged into Facebook and can't tell if they are logged into
+// //    your app or not.
+// //
+// // These three cases are handled in the callback function.
+//
+// FB.getLoginStatus(function(response) {
+//   statusChangeCallback(response);
+// });
+//
+// };
+//
+// // Load the SDK asynchronously
+// (function(d, s, id) {
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) return;
+//   js = d.createElement(s); js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+//
+// // Here we run a very simple test of the Graph API after login is
+// // successful.  See statusChangeCallback() for when this call is made.
+// function testAPI() {
+//   console.log('Welcome!  Fetching your information.... ');
+//   FB.api('/me', function(response) {
+//     console.log(response);
+//     console.log('Successful login for: ' + response.name);
+//     document.getElementById('status').innerHTML =
+//       'Thanks for logging in, ' + response.name + '!';
+//       userName = response.name.split(/[ ,]+/);
+//       userName2 = userName[0];
+//   });
+// }
+//
 
 // GLOBAL VARIABLES
 // =====================================================================================
-var userName;
+var userName2 = "";
 var userLocation;
 var cuisineChosen;
 var businessInfo = {
@@ -128,7 +133,7 @@ function homeScreen() {
 // choose from
 function openScreen() {
   var cuisineType = $("<div class='cuisine-type'>");
-  cuisineType.html("<h1 id='cuisine-header' class='cuisine-type'> What type of cuisine " + userName + "? </h1>");
+  cuisineType.html("<h1 id='cuisine-header' class='cuisine-type'> What type of cuisine " + userName2 + "? </h1>");
   $("#main-section").append(cuisineType);
   var foodTypes = ["Italian", "Chinese", "Mediterranean", "Mexican", "Indian", "Sushi"];
   var counter = 1;
@@ -361,6 +366,15 @@ function lovePhoto() {
   returnButton.attr("type", "submit");
   returnButton.html("Choose another restaurant");
   lovePhotoDiv.append(returnButton);
+
+  //facebook like button and share button rendering
+  // var facebookDiv = $("<div>");
+  // facebookDiv.addClass("fb-like");
+  // facebookDiv.attr("href", "https://teamlion7.github.io/foodMood_app-/");
+  // facebookDiv.attr("data-layout", "standard");
+  // facebookDiv.attr("data-action", "like");
+  // facebookDiv.attr("data-show-faces", "true");
+  // lovePhotoDiv.append(facebookDiv);
 
   // returns getDirections() which triggers google API directions search and embed
   getDirections();
